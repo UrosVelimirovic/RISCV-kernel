135e5a4e336992b1a773b510044610ebb914969c
Uros Velimirovic <uros1velimirovic@gmail.com>
2024-10-08 21:14:44 +0200

    novi apdejt

--- build/src/BufferKernel.d
+++
@@ -1,8 +0,0 @@
-build/src/BufferKernel.o: src/BufferKernel.cpp src/../h/BufferKernel.hpp \
- src/../h/syscall_c.hpp src/../h/../lib/hw.h
-
-src/../h/BufferKernel.hpp:
-
-src/../h/syscall_c.hpp:
-
-src/../h/../lib/hw.h:
